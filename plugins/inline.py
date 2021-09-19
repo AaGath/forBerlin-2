266744410e39f4099763fa7d07d66c5fc7b2d2c1
AaGath <89765070+AaGath@users.noreply.github.com>
2021-09-19 11:48:22 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,8 +90,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('🎬 Movies 🎬', url='https://t.me/PrimeFlixMovies'),
+            InlineKeyboardButton('📺 Tv Series 📺', url='https://t.me/TvseriesCollectionsPrimeflix')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
